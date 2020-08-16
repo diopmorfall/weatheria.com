37479d0e4750357661d8e14c0e9af3f8313eae3e
diopmorfall <morfall.papydiop99@gmail.com>
2020-08-16 21:00:25 +0200

    Gestione della form (inserimento e submit) senza API

--- src/index.js
+++ src/index.js
@@ -8,6 +8,38 @@ function component() {
 
 document.body.appendChild(component()); */
 
+// handling the various elements
+
+const forms = document.getElementsByName("form");
+const geoButton = document.querySelectorAll(".gps-btn");
+const searchButton = document.querySelectorAll(".search-btn");
+const input = document.querySelectorAll(".search-input");
+
+
+for (let i = 0; i < forms.length; i++) {
+    const form = forms[i];
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        document.querySelector(".results").firstElementChild.innerHTML = "";
+        document.querySelector(".results").firstElementChild.innerHTML = forms[i].firstElementChild.value;
+    });
+}
+for (let i = 0; i < searchButton.length; i++) {
+    const geo = geoButton[i];
+    const search = searchButton[i];
+    geo.addEventListener('click', function(event){
+        event.preventDefault();
+        // function that finds the coordinates, and posts the matching city
+        console.log(geoButton[i].innerHTML);
+    });
+    search.addEventListener('click', function(event){
+        event.preventDefault();
+        // function that posts the input
+        console.log(this.parentNode);
+        search.parentNode.dispatchEvent("submit");
+    });
+}
+
 // creation of elements and animations of the site
 
 // chance to save favourite cities, and see their forecast directly
