9519460191a13e9a0721f10e6a0c06bd5bf6efd2
diopmorfall <morfall.papydiop99@gmail.com>
2021-12-22 13:25:44 +0100

    HTML structure and basic mobile layout finished; started adding of the images and icons; recovered the json files

---
+++ assets/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: 'scripts/index.js', //* creates the bundle from this file. At every change, run npm start to update the bundle
+    output: {
+        filename: 'main.js', //* bundle that the HTML loads
+        path: path.resolve(__dirname, 'dist') //* directory name and the folder where we're going to save the bundle
+    },
+    plugins: [ //* adding the .env that contains the API key
+        new Dotenv(
+            {
+                path: '.env'
+            }
+        )
+    ]
+}
